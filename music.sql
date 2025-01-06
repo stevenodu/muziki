3670bd4f969c57804ab85a618db863377c138f2d
stevenodu <stevenodu@gmail.com>
2025-01-06 20:09:19 +0300

    push updated db entries for msql backup file

--- music.sql
+++ music.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 06, 2025 at 07:30 PM
+-- Generation Time: Jan 06, 2025 at 08:08 PM
 -- Server version: 8.0.36-0ubuntu0.22.04.1
 -- PHP Version: 8.1.2-1ubuntu2.20
 
@@ -40,15 +40,15 @@ CREATE TABLE `songs` (
 --
 
 INSERT INTO `songs` (`id`, `img`, `name`, `artist`, `music`) VALUES
-(1, 'http://localhost/musicapp/images/kitabumarhera.jpeg', 'Chike Hera', 'Prince Indah', 'http://localhost/musicapp/music/Prince Indah - Double Double.mp3'),
-(2, 'http://localhost/musicapp/images/indah.jpg', 'Double Double', 'Prince Indah', 'http://localhost/musicapp/music/Prince Indah - Double Double.mp3'),
-(3, 'http://localhost/musicapp/images/faded.png', 'Hera Mbese', 'Prince Indah', 'http://localhost/musicapp/music/Prince Indah - Hera Mbese.mp3'),
-(4, 'http://localhost/musicapp/images/ratherbe.jpg', 'Jared Kwaga', 'Prince Indah', 'http://localhost/musicapp/music/Prince Indah - Jared Kwaga.mp3'),
-(5, 'http://localhost/musicapp/images/faded.png', 'Jatuka', 'Prince Indah', 'http://localhost/musicapp/music/Prince Indah - Jatuka.mp3'),
-(6, 'http://localhost/musicapp/images/wechehera.jpeg', 'Kwach Ogole Koke', 'Prince Indah', 'http://localhost/musicapp/music/Prince Indah - Kwach Ogolo Koke.mp3'),
-(7, 'http://localhost/musicapp/images/indah.jpg', 'Mashabiki', 'Prince Indah', 'http://localhost/musicapp/music/Prince Indah - Mashabiki.mp3'),
-(8, 'http://localhost/musicapp/images/wechehera.jpeg', 'Nyar Joluo', 'Prince Indah', 'http://localhost/musicapp/music/Prince Indah - Nyar Joluo.mp3'),
-(9, 'http://localhost/musicapp/images/nyarmigori.jpeg', 'Nyar Migori', 'Prince Indah', 'http://localhost/musicapp/music/Prince Indah - Nyar Migori.mp3');
+(1, 'http://localhost/muziki/images/kitabumarhera.jpeg', 'Chike Hera', 'Prince Indah', 'http://localhost/muziki/music/Prince Indah - Double Double.mp3'),
+(2, 'http://localhost/muziki/images/indah.jpg', 'Double Double', 'Prince Indah', 'http://localhost/muziki/music/Prince Indah - Double Double.mp3'),
+(3, 'http://localhost/muziki/images/faded.png', 'Hera Mbese', 'Prince Indah', 'http://localhost/muziki/music/Prince Indah - Hera Mbese.mp3'),
+(4, 'http://localhost/muziki/images/ratherbe.jpg', 'Jared Kwaga', 'Prince Indah', 'http://localhost/muziki/music/Prince Indah - Jared Kwaga.mp3'),
+(5, 'http://localhost/muziki/images/faded.png', 'Jatuka', 'Prince Indah', 'http://localhost/muziki/music/Prince Indah - Jatuka.mp3'),
+(6, 'http://localhost/muziki/images/wechehera.jpeg', 'Kwach Ogole Koke', 'Prince Indah', 'http://localhost/muziki/music/Prince Indah - Kwach Ogolo Koke.mp3'),
+(7, 'http://localhost/muziki/images/indah.jpg', 'Mashabiki', 'Prince Indah', 'http://localhost/muziki/music/Prince Indah - Mashabiki.mp3'),
+(8, 'http://localhost/muziki/images/wechehera.jpeg', 'Nyar Joluo', 'Prince Indah', 'http://localhost/muziki/music/Prince Indah - Nyar Joluo.mp3'),
+(9, 'http://localhost/muziki/images/nyarmigori.jpeg', 'Nyar Migori', 'Prince Indah', 'http://localhost/muziki/music/Prince Indah - Nyar Migori.mp3');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
